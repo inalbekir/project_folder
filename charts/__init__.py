403e0e40eac47313bccd31c97a24306898df4d9f
Bekir Inal <>
2024-05-15 14:52:05 +0200

    Program and Age

--- charts/__init__.py
+++ charts/__init__.py
@@ -2,11 +2,13 @@
 from .line import plot_students_per_year
 from .language import plot_language_preference_bar_chart
 from .product import plot_program_types
-from .courses import plot_students_by_period  # Make sure this is added correctly
+from .courses import plot_students_by_period_and_program
+from .age import plot_students_by_program_and_age  # New function
 
 CHARTS = {
     'Student Enrollment': plot_students_per_year,
     'Preferred Language': plot_language_preference_bar_chart,
     'Program Types': plot_program_types,
-    'Students by Period': plot_students_by_period  # Add this line for the new chart
+    'Students by Program and Period': plot_students_by_period_and_program,
+    'Students by Program and Age': plot_students_by_program_and_age  # Add the new chart
 }
---
+++ charts/age.py
@@ -0,0 +1,22 @@
+# /project_folder/charts/program_age.py
+import pandas as pd
+import plotly.express as px
+
+
+def plot_students_by_program_and_age(df, selected_years, selected_programs):
+    # Filter by educational year
+    df = df[df['Educational Year'].isin(selected_years)]
+
+    # Further filter by selected programs if provided
+    if selected_programs:
+        df = df[df['Product Name'].isin(selected_programs)]
+
+    # Group by 'Educational Year', 'Age', and 'Product Name'
+    summary = df.groupby(['Educational Year', 'Age', 'Product Name'])['StudentID'].nunique().reset_index(
+        name='Number of Students')
+
+    fig = px.bar(summary, x='Educational Year', y='Number of Students', color='Product Name', barmode='stack',
+                 title='Number of Students in Different Types of Programs Depending on Year and Age',
+                 facet_col='Age', category_orders={"Age": sorted(df['Age'].unique())})
+
+    return fig
