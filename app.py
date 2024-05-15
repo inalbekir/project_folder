60fc5e8726689ec3ff82d08e6a574bf872d72f72
Bekir Inal <>
2024-05-15 14:15:34 +0200

    Course Filter

--- app.py
+++ app.py
@@ -8,38 +8,38 @@ def main():
     df = pd.read_csv('data/updated_coderminds.csv')
 
     with st.sidebar:
-        # Sidebar for selecting charts
         selected_chart = st.selectbox("Select a chart to display", options=list(CHARTS.keys()))
 
-        # Universal filter: Select educational years
+        # Universal filter: Educational Years
         year_options = df['Educational Year'].unique().tolist()
         selected_years = st.multiselect("Select Educational Years", options=year_options, default=year_options)
 
-        # Filters specific to each chart type
-        selected_languages = selected_programs = selected_periods = []
+        # Initialize variables to store selected items
+        selected_languages = []
+        selected_programs = []
+        selected_periods = []
+
         if selected_chart == 'Preferred Language':
             language_options = df['Student Preferred Language'].unique().tolist()
-            selected_languages = st.multiselect('Select preferred languages', options=language_options, default=language_options)
-        elif selected_chart == 'Program Types':
+            selected_languages = st.multiselect('Select preferred languages', language_options, default=language_options)
+        elif selected_chart == 'Program Types' or selected_chart == 'Students by Program and Period':
             program_options = df['Product Name'].unique().tolist()
-            selected_programs = st.multiselect('Select programs', options=program_options, default=program_options)
-        elif selected_chart == 'Students by Period':
+            selected_programs = st.multiselect('Select programs', program_options, default=program_options)
+        if selected_chart == 'Students by Program and Period':
             period_options = df['Period'].unique().tolist()
-            selected_periods = st.multiselect('Select periods', options=period_options, default=period_options)
+            selected_periods = st.multiselect('Select periods', period_options, default=period_options)
 
-    # Filter the DataFrame for selected educational years
     df_filtered = df[df['Educational Year'].isin(selected_years)]
 
-    # Determine which chart function to call and with which filters
     chart_function = CHARTS[selected_chart]
     if selected_chart == 'Preferred Language':
         fig = chart_function(df_filtered, selected_languages)
     elif selected_chart == 'Program Types':
         fig = chart_function(df_filtered, selected_programs)
-    elif selected_chart == 'Students by Period':
-        fig = chart_function(df_filtered, selected_periods)
+    elif selected_chart == 'Students by Program and Period':
+        fig = chart_function(df_filtered, selected_periods, selected_programs)
     else:
-        fig = chart_function(df_filtered)  # For charts that don't need additional filters
+        fig = chart_function(df_filtered)
 
     st.plotly_chart(fig, use_container_width=True)
 
